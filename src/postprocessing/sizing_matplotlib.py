66703354d7410c2f89992fb17b807e37208e2eb1
dcradu <dcradu@uliege.be>
2020-04-29 15:48:49 +0200

    Minor updates on post-processing.

--- src/postprocessing/sizing_matplotlib.py
+++ src/postprocessing/sizing_matplotlib.py
@@ -11,9 +11,12 @@ class SizingResultsSingleNet:
     This class allows to extract results from a optimized PyPSA network.
     """
 
-    def __init__(self, network: Network):
+    def __init__(self, network: Network, start: datetime, end: datetime, resolution: str):
 
         self.net = network
+        self.start = start
+        self.end = end
+        self.resolution = resolution
 
 
     def make_plots_one_net(self):
@@ -30,14 +33,8 @@ class SizingResultsSingleNet:
                       'demand': 'black',
                       'res': 'blue'}
 
-        resolution = 'H'
-        start = datetime(2015, 1, 1, 0, 0, 0)
-        end = datetime(2015, 1, 31, 23, 0, 0)
-
         generation_t = self.net.generators_t['p']
         generation_techs = self.net.generators['type'].unique()
-
-        print('test')
 
         self.plot_selected_generation(start, end, resolution, generation_t, generation_techs, color_dict)
 
@@ -70,14 +67,17 @@ class SizingResultsSingleNet:
 
                 generation_total += generation_t_slice
 
-        ax1.plot(generation_total.index, generation_total.values, color=colors['res'], label='demand', alpha=0.5)
-        ax1.plot(demand_t_slice.index, demand_t_slice.values, color=colors['demand'], label='total res', alpha=0.5)
+        ax1.plot(generation_total.index, generation_total.values, color=colors['res'], label='total res', alpha=0.5)
+        ax1.plot(demand_t_slice.index, demand_t_slice.values, color=colors['demand'], label='demand', alpha=0.5)
         ax1.set_ylabel('Demand and RES [GW]')
+        ax1.set_ylim([0, 600])
 
         ax2 = ax1.twinx()
         ax2.plot(ccgt_t_slice.index, ccgt_t_slice.values, color=colors['ccgt'], label='ccgt', alpha=0.9)
         ax2.plot(ens_t_slice.index, ens_t_slice.values, color=colors['load'], label='ens', alpha=0.9)
         ax2.set_ylabel('CCGT and ENS [GW]')
+        ax2.set_ylim([0, 400])
+
 
         fig.autofmt_xdate()
 
@@ -96,8 +96,11 @@ class SizingResultsCompare:
     This class allows to extract results from a optimized PyPSA network.
     """
 
-    def __init__(self, first_network: Network, second_network: Network):
+    def __init__(self, first_network: Network, second_network: Network, start: datetime, end: datetime, resolution: str):
 
+        self.start = start
+        self.end = end
+        self.resolution = resolution
         self.first_net = first_network
         self.second_net = second_network
 
@@ -116,10 +119,6 @@ class SizingResultsCompare:
                       'demand': 'black',
                       'res': 'blue'}
 
-        resolution = 'H'
-        start = datetime(2017, 1, 1, 0, 0, 0)
-        end = datetime(2017, 1, 30, 23, 0, 0)
-
         self.plot_res_profiles(start, end, resolution, color_dict)
 
     def plot_res_profiles(self, start, end, resolution, colors):
@@ -129,31 +128,65 @@ class SizingResultsCompare:
         first_generation_t = self.first_net.generators_t['p_max_pu']
         second_generation_t = self.second_net.generators_t['p_max_pu']
 
+        timeseries_first = self.first_net.generators_t['p']
+        timeseries_second = self.second_net.generators_t['p']
+
         fig, ax1 = plt.subplots()
 
-        for item in ['wind']:
+        generation_total = pd.Series(0., index=first_generation_t.index)
+        for item in techs:
 
-            if item in ['wind']:
+            if item in ['wind_onshore', 'wind_offshore', 'wind_floating', 'pv_residential', 'pv_utility']:
+
+                generation_t = timeseries_first.loc[:, timeseries_first.columns.str.contains(item)].resample(resolution).sum()
+                generation_t_per_tech = generation_t.sum(axis=1)
+                generation_t_slice = generation_t_per_tech[start:end]
+
+                generation_total += generation_t_slice
+
+        ax1.plot(generation_total.index, generation_total.values, color='red', alpha=0.3)
+
+        generation_total = pd.Series(0., index=second_generation_t.index)
+        for item in techs:
+
+            if item in ['wind_onshore', 'wind_offshore', 'wind_floating', 'pv_residential', 'pv_utility']:
+
+                generation_t = timeseries_second.loc[:, timeseries_second.columns.str.contains(item)].resample(resolution).sum()
+                generation_t_per_tech = generation_t.sum(axis=1)
+                generation_t_slice = generation_t_per_tech[start:end]
+
+                generation_total += generation_t_slice
+
+        ax1.plot(generation_total.index, generation_total.values, color='blue', alpha=0.3)
+
+        ax1.set_ylabel('RES [GW]')
+        ax1.set_ylim([0, 600])
+
+        ax2 = ax1.twinx()
+
+        for item in ['wind_offshore']:
+
+            if item in ['wind_offshore']:
 
                 generation_t = first_generation_t.loc[:, first_generation_t.columns.str.contains(item)].resample(resolution).sum()
                 generation_t_per_tech = generation_t.sum(axis=1)
                 generation_t_slice = generation_t_per_tech[start:end]
 
-                ax1.plot(generation_t_slice.index, generation_t_slice.values,
-                         color='red', label=item+'_'+str(first_strategy), alpha=0.5, linestyle='--')
+                ax2.plot(generation_t_slice.index, generation_t_slice.values,
+                         color='red', label=item+'_'+str(first_strategy), alpha=0.6, linestyle='--')
 
                 generation_t = second_generation_t.loc[:, second_generation_t.columns.str.contains(item)].resample(resolution).sum()
                 generation_t_per_tech = generation_t.sum(axis=1)
                 generation_t_slice = generation_t_per_tech[start:end]
 
-                ax1.plot(generation_t_slice.index, generation_t_slice.values,
-                         color='blue', label=item+'_'+str(second_strategy), alpha=0.5, linestyle='-')
+                ax2.plot(generation_t_slice.index, generation_t_slice.values,
+                         color='blue', label=item+'_'+str(second_strategy), alpha=0.6, linestyle='--')
 
-        ax1.set_ylabel('RES in-feed [sum of p.u.]')
+        ax2.set_ylabel('RES in-feed [sum of p.u.]')
 
         fig.autofmt_xdate()
 
-        ax1.legend(loc='upper right')
+        ax2.legend(loc='upper right')
         plt.show()
 
 
@@ -165,23 +198,27 @@ if __name__ == "__main__":
 
     results = 'compare' # 'compare', 'single'
 
+    resolution = 'H'
+    start = datetime(2015, 12, 1, 0, 0, 0)
+    end = datetime(2015, 12, 30, 23, 0, 0)
+
     if results == 'single':
 
-        run_id = '20200418_224238'
+        run_id = '20200428_162317'
 
         output_dir = f"{main_output_dir}{run_id}/"
 
         net = Network()
         net.import_from_csv_folder(output_dir)
 
-        pprp = SizingResultsSingleNet(net)
+        pprp = SizingResultsSingleNet(net, start, end, resolution)
 
         pprp.make_plots_one_net()
 
     else:
 
-        first_run_id = '20200419_130741'
-        second_run_id = '20200419_143433'
+        first_run_id = '20200428_160902'
+        second_run_id = '20200428_162317'
 
         first_output_dir = f"{main_output_dir}{first_run_id}/"
         second_output_dir = f"{main_output_dir}{second_run_id}/"
@@ -199,6 +236,6 @@ if __name__ == "__main__":
         second_net = Network()
         second_net.import_from_csv_folder(second_output_dir)
 
-        pprp = SizingResultsCompare(first_net, second_net)
+        pprp = SizingResultsCompare(first_net, second_net, start, end, resolution)
 
         pprp.make_plots_compare()
