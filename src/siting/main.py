de7315f108667c75a5302f4da54d5147959aa22f
dcradu <dcradu@uliege.be>
2020-04-29 19:53:35 +0200

    Update on resite main (multiprocessing guard).

--- src/siting/main.py
+++ src/siting/main.py
@@ -11,29 +11,32 @@ params = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
 
 tech_config_path = join(dirname(abspath(__file__)), '../parameters/pv_wind_tech_configs.yml')
 tech_config = yaml.load(open(tech_config_path), Loader=yaml.FullLoader)
-logger.info('Building class.')
-resite = Resite(params["regions"], params["technologies"], tech_config, params["timeslice"],
-                params["spatial_resolution"])
 
-logger.info('Reading input.')
-# resite.build_input_data(params["use_ex_cap"], params['filtering_layers'])
-resite.build_input_data_new(params["use_ex_cap"])
+if __name__ == '__main__':
 
-values = [0.1]
-for v in values:
-    # params['formulation_params'] = [v]
-    output_folder = None  # f"/home/utilisateur/Global_Grid/code/py_ggrid/output/resite/{v}/"
-    logger.info('Model being built.')
-    resite.build_model(params["modelling"], params['formulation'], params['formulation_params'],
-                       params['write_lp'], output_folder)
+    logger.info('Building class.')
+    resite = Resite(params["regions"], params["technologies"], tech_config, params["timeslice"],
+                    params["spatial_resolution"])
 
-    logger.info('Sending model to solver.')
-    results = resite.solve_model()
+    logger.info('Reading input.')
+    # resite.build_input_data(params["use_ex_cap"], params['filtering_layers'])
+    resite.build_input_data_new(params["use_ex_cap"])
 
-    logger.info('Retrieving results.')
-    if params["modelling"] != "pyomo" or \
-            (params["modelling"] == "pyomo" and str(results.solver.termination_condition) != "infeasible"):
-        resite.retrieve_solution()
-        resite.retrieve_sites_data()
+    values = [0.1]
+    for v in values:
+        # params['formulation_params'] = [v]
+        output_folder = None  # f"/home/utilisateur/Global_Grid/code/py_ggrid/output/resite/{v}/"
+        logger.info('Model being built.')
+        resite.build_model(params["modelling"], params['formulation'], params['formulation_params'],
+                           params['write_lp'], output_folder)
 
-    # resite.save(output_folder)
+        logger.info('Sending model to solver.')
+        results = resite.solve_model()
+
+        logger.info('Retrieving results.')
+        if params["modelling"] != "pyomo" or \
+                (params["modelling"] == "pyomo" and str(results.solver.termination_condition) != "infeasible"):
+            resite.retrieve_solution()
+            resite.retrieve_sites_data()
+
+        # resite.save(output_folder)
