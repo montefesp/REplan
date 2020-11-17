907213fce22b4954e4cb24ec490b04a8facd93cf
duboisa1 <antoine-dubois@hotmail.com>
2020-11-17 15:31:44 +0100

    Updated load shed (onshore/region -> onshore_regoin)

--- network/globals/load_shed.py
+++ network/globals/load_shed.py
@@ -11,13 +11,13 @@ logging.basicConfig(level=logging.WARNING, format="%(levelname)s %(asctime)s - %
 logger = logging.getLogger()
 
 
-def add_load_shedding(network: pypsa.Network, load_df: pd.DataFrame) -> pypsa.Network:
+def add_load_shedding(net: pypsa.Network, load_df: pd.DataFrame) -> pypsa.Network:
     """
     Adding dummy-generators for load shedding.
 
     Parameters
     ----------
-    network: pypsa.Network
+    net: pypsa.Network
         A Network instance with regions
     load_df: pd.DataFrame
         Frame containing load data.
@@ -31,21 +31,21 @@ def add_load_shedding(network: pypsa.Network, load_df: pd.DataFrame) -> pypsa.Ne
     tech_dir = f"{data_path}technologies/"
     fuel_info = pd.read_excel(join(tech_dir, 'fuel_info.xlsx'), sheet_name='values', index_col=0)
 
-    onshore_bus_indexes = network.buses[network.buses.onshore].index
+    onshore_buses = net.buses.dropna(subset=["onshore_region"], axis=0)
 
     # Get peak load and normalized load profile
     loads_max = load_df.max(axis=0)
     loads_pu = load_df.apply(lambda x: x/x.max(), axis=0)
     # Add generators for load shedding (prevents the model from being infeasible)
 
-    network.madd("Generator",
-                 "Load shed " + onshore_bus_indexes,
-                 bus=onshore_bus_indexes,
-                 type="load",
-                 p_nom=loads_max.values,
-                 p_max_pu=loads_pu.values,
-                 x=network.buses.loc[onshore_bus_indexes].x.values,
-                 y=network.buses.loc[onshore_bus_indexes].y.values,
-                 marginal_cost=fuel_info.loc["load", "cost"])
+    net.madd("Generator",
+             "Load shed " + onshore_buses.index,
+             bus=onshore_buses.index,
+             type="load",
+             p_nom=loads_max.values,
+             p_max_pu=loads_pu.values,
+             x=net.buses.loc[onshore_buses.index].x.values,
+             y=net.buses.loc[onshore_buses.index].y.values,
+             marginal_cost=fuel_info.loc["load", "cost"])
 
     return network
